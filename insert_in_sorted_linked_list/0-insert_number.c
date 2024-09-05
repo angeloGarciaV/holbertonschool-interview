50d73fd9bafee1eeba0dd7d46247c1de784f6f75
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-05 09:36:57 -0400

    refactor function to insert a number into a sorted singly linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,35 +1,41 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to pointer of first node of listint_t list
- * @number: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * insert_node -  inserts a number into a sorted singly linked list.
+ * @head: pointer to the head node pointer.
+ * @number: integer value of the number to add to the list.
+ * Return: The address of the new node, or NULL if insertion failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
+	listint_t *current = NULL;
+	listint_t *node = NULL;
 
-    current = *head;
+	if (head == NULL)
+		return (NULL);
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+	node->n = number;
+	node->next = NULL;
 
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
+	if (*head == NULL || (*head)->n >= number)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
 
-    return (new);
+	current = *head;
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+	node->next = current->next;
+	current->next = node;
+
+	return (node);
 }
