566db003df0493bbd4df1a2cdf753486f1b949d1
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-14 20:38:46 -0400

    Betty compliance

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -9,12 +9,12 @@
  * Return: pointer to the new node or NULL on failure
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node (binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if(new == NULL)
+	if (new == NULL)
 	{
 		return (NULL);
 	}
@@ -22,6 +22,6 @@
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
-	
+
 	return (new);
 }
